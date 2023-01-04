d9d95d0ff39850c8351edab2a830123db21e16b8
TheFenix2000 <m.wos.biznes@gmail.com>
2023-01-04 22:03:43 +0100

    Paste Bootstrap Navbar & Links

---
+++ consts.js
@@ -0,0 +1,28 @@
+import { datasetGenerator, zeroPad } from './utils.js';
+
+export const COUNTRIES = [
+  'Brazylia',
+  'Francja',
+  'Niemcy',
+  'Włochy',
+  'Holandia',
+  'Norwegia',
+  'Polska',
+  'Portugalia',
+];
+
+export const YEARS = new Array(10)
+  .fill(0)
+  .map((_, i) => `20${zeroPad(13 + i, 2)}r`);
+
+export const CHART_1_DATASETS = [
+  datasetGenerator('Najniższa W. Rynkowa', COUNTRIES.length, 5000, 10000),
+  datasetGenerator('Najwyższa W. Rynkowa', COUNTRIES.length, 5000, 100000),
+];
+
+export const CHART_2_DATASETS = [
+  datasetGenerator('Brazylia', YEARS.length, 1, 5),
+  datasetGenerator('Francja', YEARS.length, 3, 10),
+  datasetGenerator('Niemcy', YEARS.length, 2, 10),
+  datasetGenerator('Włochy', YEARS.length, 1, 7),
+];
---
+++ utils.js
@@ -0,0 +1,29 @@
+export const zeroPad = (num, places) => String(num).padStart(places, '0');
+
+export const randomValue = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const datasetGenerator = (label, num, minVal, maxVal) => ({
+  label,
+  data: new Array(num).fill(0).map((_) => randomValue(minVal, maxVal)),
+  borderWidth: 1,
+});
+
+export const chartBuilder = (ctx, type, labels, datasets) =>
+  new Chart(ctx, {
+    type,
+    data: {
+      labels,
+      datasets,
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
